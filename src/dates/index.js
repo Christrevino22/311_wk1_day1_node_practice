9d3420740530df4a00e75b410c28919a6c0b8fc9
Chris Trevino <chrisdadef@gmail.com>
2020-10-02 21:43:45 -0500

    updates

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,18 +1,26 @@
 // import moment here; use this package in each function
 
+const moment = require("moment")
+
 const today = () => {
   // write code for dates.today
+  let today = moment().format('dddd');  
 
+  return today;
+  
 }
 
 const calendar = () => {
   // write code for dates.calendar
+  let fullDate = moment().format("MMMM Do, YYYY");
+  return fullDate;
 
 }
 
 const currentTime = () => {
   // write code for dates.currentTime
-
+let theTime = moment().format('hh:mm:ss A');
+return theTime;
 }
 
 module.exports = {
--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,15 +1,28 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+if(num % 2 === 0){
+  return true
+} else {
+  return false
+}
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
+  const theSum = arr.reduce((accum, currentValue) => accum + currentValue);
+  return theSum;
 
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
+  let theArray = arr.reduce((accum, currentValue) => accum + currentValue);
+  if (theArray === sum) {
+    return true;
+  } else {
+    return false;
+  }
+
 
 }
 
